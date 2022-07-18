023596b0f751c2bc0e88c1c948eefe4d3cec1866
butrinthajrizi <butrinthaj1998@gmail.com>
2022-07-18 22:01:48 +0200

    updated

---
+++ index.js
@@ -0,0 +1,16 @@
+let checkbox = document.querySelector('input-checkbox');
+checkbox?.addEventListener('click', checkMode)
+
+function checkMode(){
+    if (checkbox.checked){
+        darkMode();
+    } else{
+        lightMode();
+    }
+}
+function darkMode(){
+    document.body.classList.add('dark-mode');
+}
+function lightMode(){
+    document.body.classList.remove('dark-mode');
+}
